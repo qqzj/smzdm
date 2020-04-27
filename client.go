69333065d927e1a9426665fbf4c8444ac0c38089
wangchaojie <popxmz@Gmail.com>
2020-04-27 16:27:17 +0800

    记录签到和评论返回json, 用于输出报告

--- client.go
+++ client.go
@@ -49,14 +49,14 @@ func (c *cracker) handle(method, url, referer string, body *io.Reader) []byte {
 	return content
 }
 func (c *cracker) getPostID() []int {
-	postID := make([]int, 5)
+	postID := make([]int, conf.PostCommentMax)
 	mtrand := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randNum := int(30.0 + math.Round(mtrand.Float64()*40.0))
 	url := "https://faxian.smzdm.com/h1s0t0f37c0p" + strconv.Itoa(randNum)
 	content := c.handle("GET", url, "https://www.smzdm.com/jingxuan/", nil)
 	reg, err := regexp.Compile(`articleid="\d_(\d+)"`)
 	checkError(err)
-	postIDMatch := reg.FindAllSubmatch(content, 5)
+	postIDMatch := reg.FindAllSubmatch(content, conf.PostCommentMax)
 	for k, v := range postIDMatch {
 		for k2, v2 := range v {
 			if k2 == 1 {
@@ -70,9 +70,7 @@ func (c *cracker) getPostID() []int {
 }
 
 func (c *cracker) smzdmSign() (bool, error) {
-	jsonData := &struct {
-		ErrorCode int `json:"error_code"`
-	}{}
+	jsonData := &signJson{}
 	ts := time.Now().UnixNano()
 	url := fmt.Sprintf("https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=jQuery112409568846254764496_%d&_=%d", ts, ts)
 	content := c.handle("GET", url, "http://www.smzdm.com/qiandao/", nil)
@@ -80,15 +78,14 @@ func (c *cracker) smzdmSign() (bool, error) {
 	jsonStr := reg.ReplaceAll(content, []byte(`$1`))
 	json.Unmarshal(jsonStr, jsonData)
 	if jsonData.ErrorCode == 0 {
+		signResult = append(signResult, *jsonData)
 		return true, nil
 	}
 	return false, errors.New(string(jsonStr))
 }
 
 func (c *cracker) smzdmCommit(postID int, comment string) (bool, error) {
-	jsonData := &struct {
-		ErrorCode int `json:"error_code"`
-	}{}
+	jsonData := &commentJson{}
 	ts := time.Now().UnixNano()
 	url := fmt.Sprintf("https://zhiyou.smzdm.com/user/comment/ajax_set_comment?callback=jQuery111006551744323225079_%d&type=3&pid=%d&parentid=0&vote_id=0&vote_type=&vote_group=&content=%s&_=%d", ts, postID, comment, ts)
 	content := c.handle("GET", url, "https://zhiyou.smzdm.com/user/submit/", nil)
@@ -96,6 +93,7 @@ func (c *cracker) smzdmCommit(postID int, comment string) (bool, error) {
 	jsonStr := reg.ReplaceAll(content, []byte(`$1`))
 	json.Unmarshal(jsonStr, jsonData)
 	if jsonData.ErrorCode == 0 {
+		commentResult = append(commentResult, *jsonData)
 		return true, nil
 	}
 	return false, errors.New(string(jsonStr))
