b9325c30bbf6c6582642c47c37ebb9fd93bbfb50
wangchaojie <popxmz@Gmail.com>
2020-04-27 16:27:35 +0800

    SMTP邮件服务发送每日报告

--- mail.go
+++ mail.go
@@ -1,18 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"log"
 	"net/smtp"
 	"strings"
+	"text/template"
 	"time"
 )
 
 func send() {
+	mailConten := parseMail()
+	print(string(mailConten))
+	return
+	// 解析邮件服务器主机和端口
 	smtpHost := []byte(conf.EmailFromSMTP)[0:strings.LastIndex(conf.EmailFromSMTP, ":")]
+	// 认证
 	auth := smtp.PlainAuth("", conf.EmailFrom, conf.EmailFromPassword, string(smtpHost))
-	mail := fmt.Sprintf("From:Smzdm-Auto-Sign<%s>\nTo:%s\nSubject:%s\nContent-Type: text/plain; charset=utf-8\n\n%s", conf.EmailFrom, conf.EmailTo, "Smzdm Notify", "Hello SMZDM!!"+time.Now().Format(time.Kitchen))
-	err := smtp.SendMail(conf.EmailFromSMTP, auth, conf.EmailFrom, conf.EmailTo, []byte(mail))
+	// 发送
+	err := smtp.SendMail(conf.EmailFromSMTP, auth, conf.EmailFrom, conf.EmailTo, mailConten)
 	if err != nil {
-		panic(err)
+		checkError(err)
+	} else {
+		log.Printf("邮件发送成功%+v", conf.EmailTo)
 	}
+}
+func parseMail() []byte {
+	// 接收模板编译
+	var mailContent bytes.Buffer
+	// 模板静态编译
+	tpl, err := template.New("mail.ghtml").Funcs(template.FuncMap{
+		"join":     strings.Join,
+		"dateTime": time.Now().Format,
+	}).ParseFiles("mail.ghtml")
+	checkError(err)
+	// 传入参数
+	err = tpl.Execute(&mailContent, struct {
+		Conf          config
+		Content       string
+		SignResult    []signJson
+		CommentResult []commentJson
+	}{
+		Conf:          *conf,
+		Content:       "Hello SMZDM!!" + time.Now().Format(time.Kitchen),
+		SignResult:    signResult,
+		CommentResult: commentResult,
+	})
+	checkError(err)
+	// 返回
+	return mailContent.Bytes()
 }
